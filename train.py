78fbf5c896bc45553b8fb1931f3bb63c75b2b9cc
ckodser <arshia.soltani2@gmail.com>
2022-01-20 13:10:49 +0330

    use PCA instead of random projection

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from tensorboardX import SummaryWriter
 from data import DataScheduler
 import torch.optim as optim
 from models.our_diva.our_diva import OurDIVA
+from sklearn.decomposition import PCA
 
 MODEL = {
     "OurDIVA": OurDIVA
@@ -242,17 +243,18 @@ def show_batch(dd, xx, y, t):
 
 
 def project_to_2d(loc):
-    return torch.matmul(loc, projection_matrix[:loc.size(1)]).cpu().numpy()
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(loc.cpu().numpy())
+    return principalComponents
+    # return torch.matmul(loc, projection_matrix[:loc.size(1)]).cpu().numpy()
 
 
-def plot_latent_variable(model, scheduler: DataScheduler, data_loader: DataLoader, domain_id, subplotnum):
+def get_latent_variable(model, scheduler: DataScheduler, data_loader: DataLoader, domain_id, subplotnum):
     # use the right data loader
     y_eye = torch.eye(scheduler.class_num, device=scheduler.device)
     d_eye = torch.eye(scheduler.domain_num, device=scheduler.device)
 
     for step, (x, y, d) in enumerate(data_loader):
-        if step > 2:
-            break
         # To device
         x, y, d = x.to(scheduler.device), y.to(scheduler.device), d.to(scheduler.device)
 
@@ -263,27 +265,8 @@ def plot_latent_variable(model, scheduler: DataScheduler, data_loader: DataLoade
         _, d_hat, y_hat, (_, _), (zd_p_loc, _), zd_q, (_, _) \
             , zx_q, (_, _), zy_q = model(d_onehot, x)
         zy_p_loc, _ = model.pzy(y_onehot)
-        zy_q = project_to_2d(zy_q)
-        zy_p = project_to_2d(zy_p_loc)
-        zd_q = project_to_2d(zd_q)
 
-        plt.figure(domain_id)
-        plt.scatter(x=zy_q[:, 0], y=zy_q[:, 1], c=y.cpu().float().numpy(), cmap=plt.cm.tab20, vmin=0,
-                    vmax=model.y_dim - 1)
-        plt.scatter(x=zy_p[:, 0], y=zy_p[:, 1], c=y.cpu().float().numpy(), s=np.ones(zy_p_loc.size(0)) * 200,
-                    cmap=plt.cm.tab20, vmin=0, vmax=model.y_dim - 1, marker='X', edgecolors='black')
-
-        plt.figure(subplotnum - 3)
-        plt.scatter(x=zy_q[:, 0], y=zy_q[:, 1], c=d.cpu().float().numpy(), cmap=plt.cm.tab20, vmin=0,
-                    vmax=model.d_dim - 1)
-
-        plt.figure(subplotnum - 2)
-        plt.scatter(x=zd_q[:, 0], y=zd_q[:, 1], c=d.cpu().float().numpy(), cmap=plt.cm.tab20, vmin=0,
-                    vmax=model.d_dim - 1)
-
-        plt.figure(subplotnum - 1)
-        plt.scatter(x=zd_q[:, 0], y=zd_q[:, 1], c=y.cpu().float().numpy(), cmap=plt.cm.tab20, vmin=0,
-                    vmax=model.y_dim - 1)
+        return zy_p_loc, zy_q, zd_p_loc, zd_q, y, d
 
 
 def save_latent_variable(model, scheduler: DataScheduler, writer: SummaryWriter, step):
@@ -304,24 +287,56 @@ def save_latent_variable(model, scheduler: DataScheduler, writer: SummaryWriter,
         plt.figure(subplotnum - 1)
         plt.clf()
         plt.title("domain latent, color=class")
-        ##############
 
+        ########################################
+        all_zy_p, all_zy_q, all_zd_p, all_zd_q, all_y, all_d = [], [], [], [], [], []
         for i, eval_related in enumerate(scheduler.eval_data_loaders):
             eval_data_loader, description, start_from = eval_related
             if scheduler.stage >= start_from:
-                plot_latent_variable(model, scheduler, eval_data_loader, i, subplotnum)
+                zy_p, zy_q, zd_p, zd_q, y, d = get_latent_variable(model, scheduler, eval_data_loader, i, subplotnum)
+                all_zy_q.append(zy_q)
+                all_zy_p.append(zy_p)
+                all_zd_p.append(zd_p)
+                all_zd_q.append(zd_q)
+                all_y.append(y)
+                all_d.append(d)
 
-        #########################################
-        # domain prior zd
+                plt.figure(i)
+                zy = project_to_2d(torch.cat([zy_q, zy_p], dim=0))
+
+                zy_q = zy[:zy.shape[0] // 2]
+                zy_p = zy[zy.shape[0] // 2:]
+
+                plt.scatter(x=zy_q[:, 0], y=zy_q[:, 1], c=y.cpu().numpy(), cmap=plt.cm.tab20, vmin=0,
+                            vmax=model.y_dim - 1)
+                plt.scatter(x=zy_p[:, 0], y=zy_p[:, 1], c=y.cpu().numpy(), s=np.ones(zy_p.shape[0]) * 200,
+                            cmap=plt.cm.tab20, vmin=0, vmax=model.y_dim - 1, marker='X', edgecolors='black')
+
+        ######################################
+        y = torch.cat(all_y, dim=0).cpu().numpy()
+        d = torch.cat(all_d, dim=0).cpu().numpy()
+
+        zy = project_to_2d(torch.cat(all_zy_q + all_zy_p, dim=0))
+        zy_q = zy[:zy.shape[0] // 2]
+        zy_p = zy[zy.shape[0] // 2:]
+
+        zd = project_to_2d(torch.cat(all_zd_q + all_zd_p, dim=0))
+        zd_q = zd[:zd.shape[0] // 2]
+        zd_p = zd[zd.shape[0] // 2:]
+
+        plt.figure(subplotnum - 3)
+        plt.scatter(x=zy_q[:, 0], y=zy_q[:, 1], c=d, cmap=plt.cm.tab20, vmin=0,
+                    vmax=model.d_dim - 1)
+
         plt.figure(subplotnum - 2)
-        d = torch.arange(0, model.d_dim, device=scheduler.device)
-        d_eye = torch.eye(scheduler.domain_num, device=scheduler.device)
-        d_onehot = d_eye[d]
-        zd_p_loc, _ = model.pzd(d_onehot)
-        zd_p = project_to_2d(zd_p_loc)
-        plt.scatter(x=zd_p[:, 0], y=zd_p[:, 1], c=d.cpu().float().numpy(), s=np.ones(zd_p_loc.size(0)) * 200,
+        plt.scatter(x=zd_q[:, 0], y=zd_q[:, 1], c=d, cmap=plt.cm.tab20, vmin=0,
+                    vmax=model.d_dim - 1)
+        plt.scatter(x=zd_p[:, 0], y=zd_p[:, 1], c=d, s=np.ones(zd_p.shape[0]) * 200,
                     cmap=plt.cm.tab20, vmin=0, vmax=model.d_dim - 1, marker='X', edgecolors='black')
-        #########################################
+
+        plt.figure(subplotnum - 1)
+        plt.scatter(x=zd_q[:, 0], y=zd_q[:, 1], c=y, cmap=plt.cm.tab20, vmin=0,
+                    vmax=model.y_dim - 1)
 
         for domain_id in range(len(scheduler.eval_data_loaders)):
             fig = plt.figure(domain_id)
