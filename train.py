b5e455a1ded8a1cd4cbc2995cc00cef776a06d94
mryf <mohammadreza.yazdanifar75@gmail.com>
2022-02-02 16:10:47 +0330

    CalOff model bug fix.

--- train.py
+++ train.py
@@ -7,11 +7,14 @@ import torch
 from tensorboardX import SummaryWriter
 from data import DataScheduler
 import torch.optim as optim
+
+from models.our_diva.CalOff import CalOff
 from models.our_diva.our_diva import OurDIVA
 import monitoring
 
 MODEL = {
-    "OurDIVA": OurDIVA
+    "OurDIVA": OurDIVA,
+    "CalOff": CalOff
 }
 
 
