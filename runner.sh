41078516ab499726ccb8b985476743694e993022
mryf <->
2022-02-25 17:20:58 +0330

    testing for diva scenario changed

--- runner.sh
+++ runner.sh
@@ -1,12 +1,12 @@
 #cd /home/mryf/PycharmProjects/dgcl
 #source venv/bin/activate
-python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=0
-python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=1
+python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=0 &
+python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=1 &
 python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=2
-python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=3
-python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=4
+python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=3 &
+python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=4 &
 python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=5
-python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=6
-python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=7
+python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=6 &
+python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=7 &
 python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=8
 python main.py --log-dir ./logs/our_diva_no_change/ --config configs/ourdiva.yaml --episode episodes/diva_experiment/diva_unsupervised_boost.yaml --override seed=9
